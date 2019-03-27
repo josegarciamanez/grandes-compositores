b7f18ed64cc2f477a45f643890954e26adc09792
Jose García Máñez <josegarciamanez@gmail.com>
2019-03-27 12:57:31 +0100

    refactorizacion1

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link class="mr-2" to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-    </div>
+    <Nav/>
+    <Descripcion/>
     <router-view/>
   </div>
 </template>
 
 <script>
-import "./assets/payp.css";
+import "./assets/css/payp.css";
+import Descripcion from "./components/Descripcion";
+import Nav from "./components/Nav";
+import "./assets/css/main.css";
 
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    Descripcion,
+    Nav
+  }
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-.animated {
-  -webkit-animation-duration: 1s;
-  animation-duration: 1s;
-  -webkit-animation-fill-mode: both;
-  animation-fill-mode: both;
-}
-.fast {
-  -webkit-animation-duration: 0.4s;
-  animation-duration: 0.4s;
-  -webkit-animation-fill-mode: both;
-  animation-fill-mode: both;
-}
-.fadeIn {
-  animation-name: fadeIn;
-}
-@-moz-keyframes fadeIn {
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-}
-@-webkit-keyframes fadeIn {
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-}
-@-o-keyframes fadeIn {
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-}
-@keyframes fadeIn {
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-}
-</style>
---
+++ src/components/Descripcion.vue
@@ -0,0 +1,5 @@
+<template functional>
+  <div class="mt-5">
+    <h1>Descripcion Works...</h1>
+  </div>
+</template>
